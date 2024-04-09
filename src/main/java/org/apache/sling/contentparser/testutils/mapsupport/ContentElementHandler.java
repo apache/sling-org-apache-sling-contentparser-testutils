044f9aac4e8f29490137a88a94dff2b6bae8182c
Stefan Seifert <stefanseifert@users.noreply.github.com>
2024-04-09 10:32:57 +0200

    SLING-12281 apply spotless code formatting

--- src/main/java/org/apache/sling/contentparser/testutils/mapsupport/ContentElementHandler.java
+++ src/main/java/org/apache/sling/contentparser/testutils/mapsupport/ContentElementHandler.java
@@ -1,21 +1,21 @@
-/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- ~ Licensed to the Apache Software Foundation (ASF) under one
- ~ or more contributor license agreements.  See the NOTICE file
- ~ distributed with this work for additional information
- ~ regarding copyright ownership.  The ASF licenses this file
- ~ to you under the Apache License, Version 2.0 (the
- ~ "License"); you may not use this file except in compliance
- ~ with the License.  You may obtain a copy of the License at
- ~
- ~   http://www.apache.org/licenses/LICENSE-2.0
- ~
- ~ Unless required by applicable law or agreed to in writing,
- ~ software distributed under the License is distributed on an
- ~ "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- ~ KIND, either express or implied.  See the License for the
- ~ specific language governing permissions and limitations
- ~ under the License.
- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.sling.contentparser.testutils.mapsupport;
 
 import java.util.Map;
@@ -29,7 +29,7 @@ import org.apache.sling.contentparser.api.ContentHandler;
  * {@link ContentHandler} implementation that produces a tree of {@link ContentElement} items.
  */
 public final class ContentElementHandler implements ContentHandler {
-    
+
     private ContentElement root;
     private static final Pattern PATH_PATTERN = Pattern.compile("^((/[^/]+)*)(/([^/]+))$");
 
@@ -37,8 +37,7 @@ public final class ContentElementHandler implements ContentHandler {
     public void resource(String path, Map<String, Object> properties) {
         if (StringUtils.equals(path, "/")) {
             root = new ContentElement(null, properties);
-        }
-        else {
+        } else {
             if (root == null) {
                 throw new ContentElementHandlerException("Root resource not set.");
             }
@@ -51,8 +50,7 @@ public final class ContentElementHandler implements ContentHandler {
             ContentElement parent;
             if (StringUtils.isEmpty(relativeParentPath)) {
                 parent = root;
-            }
-            else {
+            } else {
                 parent = root.getChild(relativeParentPath);
             }
             if (parent == null) {
@@ -61,9 +59,8 @@ public final class ContentElementHandler implements ContentHandler {
             parent.getChildren().put(name, new ContentElement(name, properties));
         }
     }
-    
+
     public ContentElement getRoot() {
         return root;
     }
-
 }
